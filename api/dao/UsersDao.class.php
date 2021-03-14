b31de44182beab316cd8aa66b3e58e0693054029
nicolae.topala <nicolae.topala13@gmail.com>
2021-03-14 14:16:39 +0100

    add query method

--- api/dao/UsersDao.class.php
+++ api/dao/UsersDao.class.php
@@ -1,9 +1,15 @@
 <?php
 
+  require_once dirname(__FILE__)."/BaseDao.class.php";
+
   class UsersDao extends BaseDao{
 
     public function get_user_by_email($email){
+      return $this->query_unique("SELECT * FROM users WHERE email = :email", ["email" => $email]);
+    }
 
+    public function get_user_by_id($id){
+      return $this->query_unique("SELECT * FROM users WHERE id = :id", ["id" => $id]);
     }
 
   }
---
+++ api/test.php
@@ -0,0 +1,18 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+error_reporting(E_ALL);
+
+  require_once dirname(__FILE__)."/dao/UsersDao.class.php";
+
+  $user_dao = new UsersDao();
+
+  $user = $user_dao->get_user_by_id("1");
+
+  print_r($user);
+
+
+
+?>
