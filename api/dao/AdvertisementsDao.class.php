f1f9af91af1d3d1dda3e52abc2c6f0dabcb6556c
nicolae.topala <nicolae.topala13@gmail.com>
2021-03-14 20:57:39 +0100

    add insert and update method for advertisement

--- api/dao/AdvertisementsDao.class.php
+++ api/dao/AdvertisementsDao.class.php
@@ -1,9 +1,15 @@
 <?php
 
+  require_once dirname(__FILE__)."/BaseDao.class.php";
+
   class AdvertisementsDao extends BaseDao{
 
-    public function add_advertisement($admin_id,$name){
+    public function add_advertisement($advertisement){
+      return $this->insert("advertisements", $advertisement);
+    }
 
+    public function update_advertisement($id, $advertisement){
+      $this->update("advertisements", $id, $advertisement);
     }
 
   }
--- api/test.php
+++ api/test.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 
 error_reporting(E_ALL);
 
-  require_once dirname(__FILE__)."/dao/UsersDao.class.php";
+/*  require_once dirname(__FILE__)."/dao/UsersDao.class.php";
 
   $user_dao = new UsersDao();
 
@@ -15,14 +15,27 @@ error_reporting(E_ALL);
     'first_name' => "213",
     'last_name' => "321",
     'password' => "123",
-    'email' => "yes",
-    'telephone' => "321"
+    'email' => "lol",
+    'telephone' => "dsadas"
   ];
 
   $user = $user_dao->add_user($user1);
 
   print_r($user);
+*/
 
+ require_once dirname(__FILE__)."/dao/AdvertisementsDao.class.php";
 
+$advertisement_dao = new AdvertisementsDao();
+
+  $advertisement1 = [
+    'admin_id' => 2,
+    'name' => "Casa de vanzare LOL",
+    'address' => "LA BUIUCANI"
+  ];
+
+  $advertisement = $advertisement_dao->update_advertisement(1,$advertisement1);
+
+  print_r($advertisement);
 
 ?>
