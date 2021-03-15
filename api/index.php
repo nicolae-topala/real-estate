023f1c278abcbcc460c370987bd5d80af7783c53
nicolae.topala <nicolae.topala13@gmail.com>
2021-03-15 19:43:58 +0100

    added comment blocks

--- api/index.php
+++ api/index.php
@@ -6,24 +6,45 @@
   require dirname(__FILE__)."/../vendor/autoload.php";
   require dirname(__FILE__)."/dao/UsersDao.class.php";
 
+  /**
+   * UserDao object replacer
+   * @example $dao = new UserDao(); -> Flight::userdao()
+   */
   Flight::register('userdao', 'UsersDao');
 
   Flight::route('/', function(){
     echo 'hello world!';
   });
 
+  /**
+   * Flight for getting all users
+   * @var [type]
+   */
   Flight::route('GET /users', function(){
     flight::json(Flight::userdao()->get_all());
   });
 
+
+  /**
+   * Flight for selecting a specific user by id
+   * Using get_by_id method from BaseDao
+   */
   Flight::route('GET /users/@id', function($id){
     flight::json(Flight::userdao()->get_by_id($id));
   });
 
+  /**
+   * Flight for Inserting data in DB
+   * Using add() method from BaseDao
+   */
   Flight::route('POST /users', function(){
     Flight::json(Flight::userdao()->add(Flight::request()->data->getData()));
   });
 
+  /**
+   * Flight for altering data in DB
+   * Using update() and get_by_id() method from BaseDao
+   */
   Flight::route('PUT /users/@id', function($id){
     Flight::userdao()->update($id, Flight::request()->data->getData());
 
