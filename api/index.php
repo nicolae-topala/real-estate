7bedd8a2a3a1255ec1fab7f01746a6fa11bb44b0
nicolae.topala <nicolae.topala13@gmail.com>
2021-03-15 19:32:47 +0100

    install flight and add some basic routes

--- api/index.php
+++ api/index.php
@@ -3,9 +3,32 @@
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
 
-  require_once dirname(__FILE__)."/dao/BaseDao.class.php";
+  require dirname(__FILE__)."/../vendor/autoload.php";
+  require dirname(__FILE__)."/dao/UsersDao.class.php";
 
-  $becirovic = new BaseDao();
-  echo "Hello from API";
+  Flight::register('userdao', 'UsersDao');
 
+  Flight::route('/', function(){
+    echo 'hello world!';
+  });
+
+  Flight::route('GET /users', function(){
+    flight::json(Flight::userdao()->get_all());
+  });
+
+  Flight::route('GET /users/@id', function($id){
+    flight::json(Flight::userdao()->get_by_id($id));
+  });
+
+  Flight::route('POST /users', function(){
+    Flight::json(Flight::userdao()->add(Flight::request()->data->getData()));
+  });
+
+  Flight::route('PUT /users/@id', function($id){
+    Flight::userdao()->update($id, Flight::request()->data->getData());
+
+    flight::json(Flight::userdao()->get_by_id($id));
+  });
+
+  Flight::start();
 ?>
