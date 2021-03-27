db896c26c3dcab29ab1d485349128154c866d192
nicolae.topala <nicolae.topala13@gmail.com>
2021-03-27 21:27:46 +0100

    switched to SMTP Gmail, couldn't fix the mailgun

--- api/config.php
+++ api/config.php
@@ -7,10 +7,10 @@
     const DB_PASSWORD = "123321";
     const DB_SCHEME = "real-estate";
 
-    const SMTP_HOST = "smtp.eu.mailgun.org";
+    const SMTP_HOST = "smtp.gmail.com";
     const SMTP_PORT = 587;
-    const SMTP_USER = "postmaster@real-estate.studio";
-    const SMTP_PASS = "94f8fe1add9ee36d1e20be1";
+    const SMTP_USER = "real.estate.project.ibu@gmail.com";
+    const SMTP_PASS = "tuw%pO*WOT*@";
   }
 
 ?>
