f3cd8d1f770fddd935d98b8fff31a1f4befebeb1
nicolae.topala <nicolae.topala13@gmail.com>
2021-05-15 20:02:32 +0200

    added pagination for search

--- assets/js/search.js
+++ assets/js/search.js
@@ -11,75 +11,75 @@ $(document).ready(function(){
         }
     });
 
-    if (window.location.search) {
-        var data = urlParamsToJson();
-        var offset = 0;
-
-        data.limit = 12;
-        data.offset = 2;
-
-        getAds(data);
-    } else {
-        alert("hehe");
-    }
+    getAds(1);
 });
 
-function doNext(data, page){
-  data.limit = 12;
-  data.offset = page + 1;
-  console.log(data);
-  $.get( "api/advertisements", data ).done(function(data){
-      var html= "";
-      for(var i = 0; i < 12; i++){
-        html += '<div class="col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3 recommended_column"><a class="ads_link" href="#">'
-              +  '<div class="recommended_div_block">'
-                +    '<p class="recommended_paragraph_title"><strong>&nbsp;'+data[i].title+'</strong></p>'
-                +    '<p class="recommended_paragraph">Price: '+data[i].price+'&nbsp;<i class="fa fa-dollar"></i></p>'
-                +    '<p class="recommended_paragraph">Space: '+data[i].space+' m<sup>2</sup></p>'
-                +    '<p class="recommended_paragraph">Address: '+data[i].address+'</p><img class="recommended_image" src="https://picsum.photos/1200/1200" width="300" height="200">'
-              +  '</div>'
-          +  '</a></div>';
-      }
-      $("#SearchResults").html(html);
-  });
-}
+function getAds(page){
+    if (window.location.search) {
+        $("#SearchPage").hide();
+        $("#SearchResults").hide();
 
-function getAds(main_data){
-  $.get( "api/advertisements", main_data ).done(function(data){
-      if(data<1){
-          var html = '<strong>There are no such results</strong>';
-          $("#ResultText").html(html);
-          $("#ResultText").show();
-          $("#SearchResults").html();
+        var main_data = urlParamsToJson();
 
-      }else{
-          var html = "";
-          var pages = Math.ceil(data.length / 12);
+        main_data.limit = 12;
+        main_data.offset = (page-1) * main_data.limit;
 
-          for(var i = 0; i < data.length; i++){
-            html += '<div class="col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3 recommended_column"><a class="ads_link" href="#">'
-                  +  '<div class="recommended_div_block">'
-                    +    '<p class="recommended_paragraph_title"><strong>&nbsp;'+data[i].title+'</strong></p>'
-                    +    '<p class="recommended_paragraph">Price: '+data[i].price+'&nbsp;<i class="fa fa-dollar"></i></p>'
-                    +    '<p class="recommended_paragraph">Space: '+data[i].space+' m<sup>2</sup></p>'
-                    +    '<p class="recommended_paragraph">Address: '+data[i].address+'</p><img class="recommended_image" src="https://picsum.photos/1200/1200" width="300" height="200">'
-                  +  '</div>'
-              +  '</a></div>';
-          }
-          $("#ResultText").show();
-          $("#SearchResults").html(html);
+        $.get( "api/advertisements", main_data ).done(function(data){
+            if(data < 1){
+                var html = '<strong>There are no such results</strong>';
+                $("#ResultText").html(html);
+                $("#ResultText").show();
+                $("#SearchResults").html();
+            }else{
+                var html = "";
 
-          html= '<a id="PagePrevious" class="page-link btn pages" aria-label="Previous"><span aria-hidden="true">«</span></a>';
-          for(var i = 1; i <= 2; i++){
-              html += '<a id="Page'+i+'" class="page-link btn pages">'+i+'</a>';
-          }
-          html +='<a id="PageNext" onclick="doNext()" class="page-link btn pages" aria-label="Next" ><span aria-hidden="true">»</span></a>';
-          $("#SearchPage").html(html);
-          $("#SearchPage").show();
+                for(var i = 0; i < data.length; i++){
+                  html += '<div class="col-sm-6 col-md-6 col-lg-3 col-xl-3 col-xxl-3 recommended_column"><a class="ads_link" href="#">'
+                        +  '<div class="recommended_div_block">'
+                          +    '<p class="recommended_paragraph_title"><strong>&nbsp;'+data[i].title+'</strong></p>'
+                          +    '<p class="recommended_paragraph">Price: '+data[i].price+'&nbsp;<i class="fa fa-dollar"></i></p>'
+                          +    '<p class="recommended_paragraph">Space: '+data[i].space+' m<sup>2</sup></p>'
+                          +    '<p class="recommended_paragraph">Address: '+data[i].address+'</p><img class="recommended_image" src="https://picsum.photos/1200/1200" width="300" height="200">'
+                        +  '</div>'
+                    +  '</a></div>';
+                }
+                $("#SearchResults").html(html);
+                $("#SearchResults").show();
+                $("#ResultText").show();
 
-          insertData("#SearchForm", main_data);
-          insertOptions('type_id', main_data.type_id);
-          insertOptions('location_id', main_data.location_id);
-      }
-  });
+                main_data.limit = 1000;
+                main_data.offset = 0;
+
+                $.get( "api/advertisements", main_data ).done(function(data){
+                    var total = data.length;
+
+                    var pages = Math.ceil(total/12);
+
+                    console.log(total);
+                    console.log(pages);
+                    $("#SearchPage").html("");
+
+                    var html = "";
+
+                    if(page > 1)
+                        html += '<a class="page-link btn pages" onclick="getAds('+(page-1)+')" aria-label="Previous"><span aria-hidden="true">«</span></a>';
+
+                    for(var i = 1; i <= pages; i++){
+                        if (i == page) html += '<a class="page-link btn pages page-active disabled">'+i+'</a>';
+                        else html += '<a class="page-link btn pages" onclick=getAds('+i+')>'+i+'</a>';
+                    }
+
+                    if(page < pages)
+                        html +='<a onclick="getAds('+(page+1)+')" class="page-link btn pages" aria-label="Next" ><span aria-hidden="true">»</span></a>';
+
+                    $("#SearchPage").html(html);
+                    $("#SearchPage").show();
+                });
+            }
+        });
+        insertData("#SearchForm", main_data);
+        insertOptions('type_id', main_data.type_id);
+        insertOptions('location_id', main_data.location_id);
+
+    }
 }
